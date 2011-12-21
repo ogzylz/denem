82033e539e38fbc610a9dd9d5895c4fb9b98f1f3
Oguz Yilmaz <oguzyilmaz123@gmail.com>
2011-12-21 16:06:44 -0500

    sdfds

--- src/mypack/Hello.java
+++ src/mypack/Hello.java
@@ -5,10 +5,13 @@ public class Hello {
 	public static void  main(String[] args){
 		System.out.println("hello");
 		System.out.println("hello2");
+		System.out.println("son Manual icin2");
+
 		System.out.println("only in branch2");
 		System.out.println("mybr");
 		System.out.println("mybr2");
 		System.out.println("son Manual icin");
+		System.out.println("son Manual icin2");
 
 		
 		
--- src/mypack/myclass.java
+++ src/mypack/myclass.java
@@ -7,6 +7,7 @@ public class myclass {
 	public static void main(String[] args){
 		System.out.println("branching added");
 		for(int g= 0 ; g<4;g++){
+			int t = 3;
 			g++; g++;
 			System.out.println("Son artik yahuu");
 		}
